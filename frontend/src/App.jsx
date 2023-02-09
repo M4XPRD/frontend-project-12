9e3525cec40ff87690c4c2380fccda4765927735
Max <ppmax@bk.ru>
2023-02-09 22:32:36 +0300

    React app cleared up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit
         {' '}
