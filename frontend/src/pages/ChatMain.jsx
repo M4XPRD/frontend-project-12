d0e9972be559625b5ebba32d54c0e001a8e40852
Max <ppmax@bk.ru>
2023-02-10 21:58:36 +0300

    Removed body tag

--- frontend/src/pages/ChatMain.jsx
+++ frontend/src/pages/ChatMain.jsx
@@ -4,24 +4,22 @@ import LoginPage from './LoginPage';
 import SignUpPage from './SignUpPage';
 
 const ChatMain = () => (
-  <body className="h-100 bg-light">
-    <div className="h-100">
-      <div className="h-100" id="chat">
-        <div className="d-flex flex-column h-100">
-          <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-            <div className="container"><a className="navbar-brand" href="/">Hexlet Chat</a></div>
-          </nav>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </div>
-        <div className="Toastify" />
+  <div className="h-100">
+    <div className="h-100" id="chat">
+      <div className="d-flex flex-column h-100">
+        <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+          <div className="container"><a className="navbar-brand" href="/">Hexlet Chat</a></div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
       </div>
+      <div className="Toastify" />
     </div>
-  </body>
+  </div>
 );
 
 export default ChatMain;
