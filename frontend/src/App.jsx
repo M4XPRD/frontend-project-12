ef66016ac8ece354d008aeee453af1ebac603f92
Max <ppmax@bk.ru>
2023-02-10 15:46:25 +0300

    Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ChatMain from './pages/ChatMain';
+
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>
-        Edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <ChatMain />
 );
 
 export default App;
