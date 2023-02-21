7d7349329e080a8aa4c415c4341ccc80c2869988
Max <ppmax@bk.ru>
2023-02-21 06:58:16 +0300

    Added text and few buttons

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,27 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
-const ChatPage = () => (
-  <div>
-    <Link to="/login">Back</Link>
-  </div>
-);
+const ChatPage = () => {
+  const navigate = useNavigate();
+  const deleteToken = () => {
+    localStorage.removeItem('authToken');
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem('authToken')) {
+      navigate('/login');
+    }
+  }, [navigate]);
+
+  return (
+    <div>
+      {localStorage.getItem('authToken') ? (<Link to="/login">Back</Link>) : null}
+      <br />
+      <br />
+      <br />
+      <button type="button" onClick={deleteToken}>Удалить токен</button>
+    </div>
+  );
+};
 
 export default ChatPage;
