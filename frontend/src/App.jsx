9b9a64345626f83b5afbfcc346a8b0f73f0e858d
Max <ppmax@bk.ru>
2023-02-21 06:57:30 +0300

    Context Provider added to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './pages/Main';
+import AuthProvider from './contexts/AuthProvider';
+// import AuthContext from './contexts/AuthContext';
 
 const App = () => (
-  <Main />
+  <AuthProvider>
+    <Main />
+  </AuthProvider>
 );
 
 export default App;
