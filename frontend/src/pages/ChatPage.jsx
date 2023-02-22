7a52e6008ca8bcd7367950cce5f2cf2b452f8937
Max <ppmax@bk.ru>
2023-02-22 22:51:51 +0300

    Step 3 completed

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,25 +1,29 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import useAuth from '../hooks/index';
 
 // eslint-disable-next-line max-len
 // Сделайте проверку существования токена в localStorage и редирект на форму входа в случае его отсутствия.
 
 const ChatPage = () => {
   const navigate = useNavigate();
+  const auth = useAuth();
+
+  console.log(auth.isLoggedIn);
 
   const deleteToken = () => {
-    localStorage.removeItem('authToken');
+    localStorage.removeItem('userInfo');
   };
 
   useEffect(() => {
-    if (!localStorage.getItem('authToken')) {
+    if (!localStorage.getItem('userInfo')) {
       navigate('/login');
     }
   }, [navigate]);
 
   return (
     <div>
-      {localStorage.getItem('authToken') ? (<Link to="/login">Back</Link>) : null}
+      {localStorage.getItem('userInfo') ? (<Link to="/login">Back</Link>) : null}
       <br />
       <br />
       <br />
