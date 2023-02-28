771854d5f5f71ff89f6bffeae6e5b018bfaf1a7b
Max <ppmax@bk.ru>
2023-02-28 14:10:43 +0300

    Components added to ChatPage

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable no-unused-vars */
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import axios from 'axios';
 import { setInfo } from '../store/chatSlice';
 import useAuth from '../hooks/index';
+import Channels from './chatPages/Channels';
+import Messages from './chatPages/Messages';
 
 const ChatPage = () => {
   const navigate = useNavigate();
@@ -33,18 +36,27 @@ const ChatPage = () => {
   };
 
   return (
-    <div>
-      {chatInfo.channels ? <div>КАНАЛЫ ЕСТЬ</div> : <p>ПУСТО</p>}
-      <br />
-      {auth.isLoggedIn && (<button type="button"><Link to="/login">Выход</Link></button>)}
-      <br />
-      <button type="button"><Link to="/login">Просто назад</Link></button>
-      <br />
-      <br />
-      <br />
-      <button type="button" onClick={deleteToken}>Удалить токен</button>
+    <div className="container h-100 my-4 overflow-hidden rounded shadow">
+      <div className="row h-100 bg-white flex-md-row">
+        <Channels />
+        <Messages />
+      </div>
     </div>
   );
+
+  // return (
+  //   <div>
+  //     {chatInfo.channels ? <div>КАНАЛЫ ЕСТЬ</div> : <p>ПУСТО</p>}
+  //     <br />
+  //     {auth.isLoggedIn && (<button type="button"><Link to="/login">Выход</Link></button>)}
+  //     <br />
+  //     <button type="button"><Link to="/login">Просто назад</Link></button>
+  //     <br />
+  //     <br />
+  //     <br />
+  //     <button type="button" onClick={deleteToken}>Удалить токен</button>
+  //   </div>
+  // );
 };
 
 export default ChatPage;
