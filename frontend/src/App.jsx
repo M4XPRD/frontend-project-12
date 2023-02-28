2bf81747f820a4a6d28a34eb290e3e39c13850f1
Max <ppmax@bk.ru>
2023-02-28 14:10:18 +0300

    Commented path deleted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './pages/Main';
 import AuthProvider from './contexts/AuthProvider';
-// import AuthContext from './contexts/AuthContext';
 
 const App = () => (
   <AuthProvider>
