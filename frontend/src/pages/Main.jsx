edb1dda99549aea58f7952e9c73928396b798906
Max <ppmax@bk.ru>
2023-03-01 05:34:06 +0300

    Private routing changed

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import {
-  Routes, Route, useLocation, Navigate,
+  Routes, Route, useLocation, useNavigate, Navigate,
 } from 'react-router-dom';
 import { Nav } from 'react-bootstrap';
 import ErrorPage from './ErrorPage';
@@ -9,41 +9,61 @@ import LoginPage from './LoginPage';
 import useAuth from '../hooks/index';
 
 const PrivateRoute = ({ children }) => {
-  const auth = useAuth();
+  const hasToken = localStorage.getItem('userInfo');
   const location = useLocation();
 
-  return (
-    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
+  return hasToken ? (
+    children
+  ) : (
+    <Navigate to="/login" state={{ from: location }} />
   );
 };
 
-const Main = () => (
-  <div className="h-100">
-    <div className="h-100" id="chat">
-      <div className="d-flex flex-column h-100">
-        <Nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-          <div className="container">
-            <a className="navbar-brand" href="/">Hexlet Chat</a>
-          </div>
-        </Nav>
-        <Routes>
-          <Route path="/" element={<ChatPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="*" element={<ErrorPage />} />
-          <Route
-            path="/private"
-            element={(
-              <PrivateRoute>
-                <ChatPage />
-              </PrivateRoute>
-            )}
-          />
-        </Routes>
+const Main = () => {
+  const parseToken = JSON.parse(localStorage.getItem('userInfo'));
+  const navigate = useNavigate();
+  const auth = useAuth();
+  return (
+    <div className="h-100">
+      <div className="h-100" id="chat">
+        <div className="d-flex flex-column h-100">
+          <Nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+            <div className="container">
+              <a className="navbar-brand" href="/">
+                Hexlet Chat
+              </a>
+              {parseToken && (
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => {
+                    auth.logOut();
+                    navigate('/');
+                  }}
+                >
+                  Выйти
+                </button>
+              )}
+            </div>
+          </Nav>
+          <Routes>
+            <Route
+              path="/"
+              element={(
+                <PrivateRoute>
+                  <ChatPage />
+                </PrivateRoute>
+              )}
+            />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </div>
+        <div className="Toastify" />
       </div>
-      <div className="Toastify" />
     </div>
-  </div>
-);
+  );
+};
 
 export default Main;
