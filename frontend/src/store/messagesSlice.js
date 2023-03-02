321a12aac67370470496e4438f897f9c90b24387
Max <ppmax@bk.ru>
2023-03-02 15:39:21 +0300

    messagesSlice created

---
+++ frontend/src/store/messagesSlice.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const messagesSlice = createSlice({
+  name: 'messages',
+  initialState: {
+    messages: [],
+  },
+  reducers: {
+    addMessage: (state, action) => {
+      state.messages.push(action.payload);
+    },
+  },
+});
+
+export const { addMessage } = messagesSlice.actions;
+export default messagesSlice.reducer;
