81f4ecf61e12aaa1aa227116de2525d50a0526c7
Max <ppmax@bk.ru>
2023-03-02 15:39:32 +0300

    Added 2 more reducers

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chatReducer from './chatSlice';
+import channelReducer from './channelSlice';
+import messagesReducer from './messagesSlice';
 
 const store = configureStore({
   reducer: {
     chat: chatReducer,
+    channel: channelReducer,
+    messages: messagesReducer,
   },
 });
 
