223be0e1b5f5366cf5585a48add448b012441787
Max <ppmax@bk.ru>
2023-03-05 19:49:43 +0300

    Setting active channel

--- frontend/src/pages/chatPages/Channels.jsx
+++ frontend/src/pages/chatPages/Channels.jsx
@@ -5,8 +5,13 @@ const Channels = () => {
   const channels = useSelector((state) => state.chat.chatInfo.channels);
   // [{{id: 1, name: 'general', removable: false}}
   // , {{id: 2, name: 'random', removable: false}}]
-  const activeChannel = useSelector((state) => state.channel.activeChannel);
+  const activeChannelName = useSelector((state) => state.channel.activeChannel.name);
   const dispatch = useDispatch();
+
+  const handleClick = (name, id) => {
+    const channelData = { name, id };
+    dispatch(setActiveChannel(channelData));
+  };
 
   return (
     <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
@@ -25,7 +30,7 @@ const Channels = () => {
         {channels && channels.map(({ id, name, removable }) => (removable ? (
           <li className="nav-item w-100" key={id}>
             <div role="group" className="d-flex dropdown btn-group">
-              <button onClick={() => dispatch(setActiveChannel(name))} type="button" className={`w-100 rounded-0 text-start text-truncate btn ${activeChannel === name ? 'btn-secondary' : ''}`}>
+              <button onClick={() => handleClick(name, id)} type="button" className={`w-100 rounded-0 text-start text-truncate btn ${activeChannelName === name ? 'btn-secondary' : ''}`}>
                 <span className="me-1">#</span>
                 {name}
               </button>
@@ -37,7 +42,7 @@ const Channels = () => {
           </li>
         ) : (
           <li className="nav-item w-100" key={id}>
-            <button onClick={() => dispatch(setActiveChannel(name))} type="button" className={`w-100 rounded-0 text-start btn ${activeChannel === name ? 'btn-secondary' : ''}`}>
+            <button onClick={() => handleClick(name, id)} type="button" className={`w-100 rounded-0 text-start btn ${activeChannelName === name ? 'btn-secondary' : ''}`}>
               <span className="me-1">#</span>
               {name}
             </button>
