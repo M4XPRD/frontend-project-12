1f373cbbb65a8d2b5acfaa5b51a7df076e248c79
Max <ppmax@bk.ru>
2023-03-05 19:50:35 +0300

    Comment added

--- frontend/src/store/chatSlice.js
+++ frontend/src/store/chatSlice.js
@@ -4,6 +4,7 @@ const chatSlice = createSlice({
   name: 'chat',
   initialState: {
     chatInfo: {},
+    // [{{id: 1, name: 'general', removable: false}}, {{id: 2, name: 'random', removable: false}}]
   },
   reducers: {
     setInfo: (state, action) => ({ ...state, chatInfo: action.payload }),
