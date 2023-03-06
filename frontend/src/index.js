44f8065287e7d6d060bc92a3da8447ab6dd066ab
Max <ppmax@bk.ru>
2023-03-06 21:36:18 +0300

    Socket.io added to core file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
+import { io } from 'socket.io-client';
 import App from './App';
 import store from './store';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const socket = io();
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <App socket={socket} />
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
