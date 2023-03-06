b784a03139b22b76054adcc2ee079e8382c0a3fe
Max <ppmax@bk.ru>
2023-03-06 21:36:28 +0300

    Socket Provider added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './pages/Main';
 import AuthProvider from './contexts/AuthProvider';
+import SocketProvider from './contexts/SocketProvider';
 
-const App = () => (
-  <AuthProvider>
-    <Main />
-  </AuthProvider>
+const App = ({ socket }) => (
+  <SocketProvider socket={socket}>
+    <AuthProvider>
+      <Main />
+    </AuthProvider>
+  </SocketProvider>
 );
 
 export default App;
