2488b5fdd520697f3dfe600cac80943b622166b0
Max <ppmax@bk.ru>
2023-03-07 21:31:35 +0300

    Provider fully completed

--- frontend/src/contexts/SocketProvider.jsx
+++ frontend/src/contexts/SocketProvider.jsx
@@ -2,20 +2,68 @@ import { useMemo, useCallback } from 'react';
 import SocketContext from './SocketContext';
 
 const SocketProvider = ({ socket, children }) => {
-  const getMessages = useCallback(() => {
-    socket.on('get messages', (data) => {
-      console.log(data);
+  const getMessage = useCallback(() => {
+    socket.on('newMessage', (payload) => {
+      console.log(payload); // => { body: "new message", channelId: 7, id: 8, username: "admin" }
     });
   }, [socket]);
 
-  const sendMessage = useCallback((data) => {
-    socket.emit('send message', data);
+  const sendMessage = useCallback((payload) => {
+    socket.emit('newMessage', payload);
+    // { body: "message text", channelId: 1, username: 'admin' }
+  }, [socket]);
+
+  const getChannel = useCallback(() => {
+    socket.on('newChannel', (payload) => {
+      console.log(payload); // { id: 6, name: "new channel", removable: true }
+    });
+  }, [socket]);
+
+  const sendChannel = useCallback((payload) => {
+    socket.emit('newChannel', payload);
+    // { name: "new channel" }
+  }, [socket]);
+
+  const getRemovedChannel = useCallback(() => {
+    socket.on('removeChannel', (payload) => {
+      console.log(payload); // { id: 6 };
+    });
+  }, [socket]);
+
+  const sendRemovedChannel = useCallback((payload) => {
+    socket.emit('removeChannel', payload);
+    // { id: 6 }
+  }, [socket]);
+
+  const getRenamedChannel = useCallback(() => {
+    socket.on('renameChannel', (payload) => {
+      console.log(payload); // { id: 7, name: "new name channel", removable: true }
+    });
+  }, [socket]);
+
+  const sendRenamedChannel = useCallback((payload) => {
+    socket.emit('removeChannel', payload);
+    // { id: 7, name: "new name channel", removable: true }
   }, [socket]);
 
   const providedData = useMemo(() => ({
-    getMessages,
+    getMessage,
     sendMessage,
-  }), [getMessages, sendMessage]);
+    getChannel,
+    sendChannel,
+    getRemovedChannel,
+    sendRemovedChannel,
+    getRenamedChannel,
+    sendRenamedChannel,
+  }), [
+    getMessage,
+    sendMessage,
+    getChannel,
+    sendChannel,
+    getRemovedChannel,
+    sendRemovedChannel,
+    getRenamedChannel,
+    sendRenamedChannel]);
 
   return (
     <SocketContext.Provider value={providedData}>
