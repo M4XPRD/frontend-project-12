5435440a90cc0bb63452756b5b2e61dfc9b8cbea
Max <ppmax@bk.ru>
2023-03-07 23:03:43 +0300

    newMessage socket added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useDispatch } from 'react-redux';
 import Main from './pages/Main';
 import AuthProvider from './contexts/AuthProvider';
 import SocketProvider from './contexts/SocketProvider';
+import { addMessage } from './store/messagesSlice';
 
-const App = ({ socket }) => (
-  <SocketProvider socket={socket}>
-    <AuthProvider>
-      <Main />
-    </AuthProvider>
-  </SocketProvider>
-);
+const App = ({ socket }) => {
+  const dispatch = useDispatch();
+  socket.on('newMessage', (data) => {
+    dispatch(addMessage(data));
+  });
+  return (
+    <SocketProvider socket={socket}>
+      <AuthProvider>
+        <Main />
+      </AuthProvider>
+    </SocketProvider>
+  );
+};
 
 export default App;
