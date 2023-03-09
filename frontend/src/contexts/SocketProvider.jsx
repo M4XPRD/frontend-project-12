e421d40745e4d5615e920d1b842243c4a3d0f18e
Max <ppmax@bk.ru>
2023-03-09 21:46:16 +0300

    Only emit sockets left

--- frontend/src/contexts/SocketProvider.jsx
+++ frontend/src/contexts/SocketProvider.jsx
@@ -2,21 +2,9 @@ import { useMemo, useCallback } from 'react';
 import SocketContext from './SocketContext';
 
 const SocketProvider = ({ socket, children }) => {
-  const getMessage = useCallback(() => {
-    socket.on('newMessage', (payload) => {
-      console.log(payload); // => { body: "new message", channelId: 7, id: 8, username: "admin" }
-    });
-  }, [socket]);
-
   const sendMessage = useCallback((payload) => {
     socket.emit('newMessage', payload);
     // { body: "message text", channelId: 1, username: 'admin' }
-  }, [socket]);
-
-  const getChannel = useCallback(() => {
-    socket.on('newChannel', (payload) => {
-      console.log(payload); // { id: 6, name: "new channel", removable: true }
-    });
   }, [socket]);
 
   const sendChannel = useCallback((payload) => {
@@ -24,21 +12,9 @@ const SocketProvider = ({ socket, children }) => {
     // { name: "new channel" }
   }, [socket]);
 
-  const getRemovedChannel = useCallback(() => {
-    socket.on('removeChannel', (payload) => {
-      console.log(payload); // { id: 6 };
-    });
-  }, [socket]);
-
   const sendRemovedChannel = useCallback((payload) => {
     socket.emit('removeChannel', payload);
     // { id: 6 }
-  }, [socket]);
-
-  const getRenamedChannel = useCallback(() => {
-    socket.on('renameChannel', (payload) => {
-      console.log(payload); // { id: 7, name: "new name channel", removable: true }
-    });
   }, [socket]);
 
   const sendRenamedChannel = useCallback((payload) => {
@@ -47,22 +23,14 @@ const SocketProvider = ({ socket, children }) => {
   }, [socket]);
 
   const providedData = useMemo(() => ({
-    getMessage,
     sendMessage,
-    getChannel,
     sendChannel,
-    getRemovedChannel,
     sendRemovedChannel,
-    getRenamedChannel,
     sendRenamedChannel,
   }), [
-    getMessage,
     sendMessage,
-    getChannel,
     sendChannel,
-    getRemovedChannel,
     sendRemovedChannel,
-    getRenamedChannel,
     sendRenamedChannel]);
 
   return (
