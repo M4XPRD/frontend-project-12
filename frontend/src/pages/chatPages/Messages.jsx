14df35df44e499f0154bbd91f347bfe9b49ed8a3
Max <ppmax@bk.ru>
2023-03-09 23:49:00 +0300

    Scrolling view added

--- frontend/src/pages/chatPages/Messages.jsx
+++ frontend/src/pages/chatPages/Messages.jsx
@@ -8,6 +8,7 @@ const Messages = () => {
   const [currentMessage, setCurrectMessage] = useState('');
   const socket = useSocket();
   const messageScroll = useRef(null);
+  const inputFocus = useRef();
   const activeChannel = useSelector((state) => state.channel.activeChannel);
   const allMessages = useSelector((state) => state.messages.messages);
   const activeId = activeChannel.id;
@@ -21,7 +22,8 @@ const Messages = () => {
   });
 
   useEffect(() => {
-    messageScroll.current.scrollIntoView({ behavior: 'smooth' });
+    messageScroll.current.scrollIntoView(false, { behavior: 'smooth' });
+    inputFocus.current.focus();
   }, [filteredMessages]);
 
   const handleSubmit = (e) => {
@@ -51,7 +53,7 @@ const Messages = () => {
         <div className="mt-auto px-5 py-3">
           <form noValidate="" className="py-1 border rounded-2" onSubmit={handleSubmit}>
             <div className={inputClassNames}>
-              <input name="body" aria-label="Новое сообщение" placeholder="Введите сообщение..." className="border-0 p-0 ps-2 form-control" value={currentMessage} onChange={(e) => setCurrectMessage(e.target.value)} />
+              <input name="body" aria-label="Новое сообщение" placeholder="Введите сообщение..." className="border-0 p-0 ps-2 form-control" value={currentMessage} ref={inputFocus} onChange={(e) => setCurrectMessage(e.target.value)} />
               <button type="submit" disabled={!currentMessage} style={{ borderColor: 'white' }} className="btn btn-group-vertical">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
                   <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
