8d1d17bcc62d78631fb68df7e0f1b7ac75d0fe16
Max <ppmax@bk.ru>
2023-03-14 00:37:10 +0300

    Added SetTimeout

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -8,22 +8,28 @@ import store from '../store/index';
 
 const ChatPage = () => {
   useEffect(() => {
-    axios.get('/api/v1/data', {
-      headers: {
-        Authorization: `Bearer ${JSON.parse(localStorage.getItem('userInfo')).token}`,
-      },
-    }).then((responce) => {
+    const getData = () => {
+      axios.get('/api/v1/data', {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(localStorage.getItem('userInfo')).token}`,
+        },
+      }).then((responce) => {
       // {channels: Array(2), messages: Array(0), currentChannelId: 1}
-      const { channels, messages, currentChannelId } = responce.data;
-      // console.log('КАНАЛЫ');
-      // console.log(channels);
-      // console.log('СООБЩЕНИЯ');
-      // console.log(messages);
-      console.log('ТЕКУЩИЙ ID');
-      console.log(currentChannelId);
-      store.dispatch(setInfo(channels));
-      store.dispatch(addMessages(messages));
-    }).catch((error) => console.log(error));
+        const { channels, messages, currentChannelId } = responce.data;
+        // console.log('КАНАЛЫ');
+        // console.log(channels);
+        // console.log('СООБЩЕНИЯ');
+        // console.log(messages);
+        console.log('ТЕКУЩИЙ ID');
+        console.log(currentChannelId);
+        store.dispatch(setInfo(channels));
+        store.dispatch(addMessages(messages));
+      }).catch((error) => {
+        console.log('Something wrong happened', error);
+        setTimeout(() => getData(), 5000);
+      });
+    };
+    getData();
   }, []);
 
   return (
