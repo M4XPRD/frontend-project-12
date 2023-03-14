18728dc4b1b58e78e709180c3b721d0cc548d35b
Max <ppmax@bk.ru>
2023-03-14 22:20:22 +0300

    Network status provider added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './pages/Main';
 import AuthProvider from './contexts/AuthProvider';
+import NetworkProvider from './contexts/NetworkProvider';
 import SocketProvider from './contexts/SocketProvider';
 
 const App = ({ socket }) => (
   <SocketProvider socket={socket}>
-    <AuthProvider>
-      <Main />
-    </AuthProvider>
+    <NetworkProvider>
+      <AuthProvider>
+        <Main />
+      </AuthProvider>
+    </NetworkProvider>
   </SocketProvider>
 );
 
