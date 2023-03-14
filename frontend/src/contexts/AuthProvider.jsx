4aadb968ce3a8e9768bae60055cabae2d170773e
Max <ppmax@bk.ru>
2023-03-14 22:20:29 +0300

    Comments deleted

--- frontend/src/contexts/AuthProvider.jsx
+++ frontend/src/contexts/AuthProvider.jsx
@@ -1,36 +1,6 @@
 import { useState, useMemo, useCallback } from 'react';
 import AuthContext from './AuthContext';
 
-/*
-const AuthProvider = ({ children }) => {
-  const savedUserData = JSON.parse(localStorage.getItem('user'));
-
-  const [user, setUser] = useState(savedUserData ? { username: savedUserData.username } : null);
-
-  const logIn = useCallback((userData) => {
-    localStorage.setItem('user', JSON.stringify(userData));
-    setUser({ username: userData.username });
-  }, []);
-
-  const logOut = useCallback(() => {
-    localStorage.removeItem('user');
-    setUser(null);
-  }, []);
-
-  const providedData = useMemo(() => ({
-    logIn,
-    logOut,
-    user,
-  }), [logIn, logOut, user]);
-
-  return (
-    <AuthContext value={providedData}>{children}</AuthContext>
-  );
-};
-
-export default AuthProvider;
-*/
-
 const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
