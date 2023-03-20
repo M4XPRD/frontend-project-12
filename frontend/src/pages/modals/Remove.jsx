300c172efd3c34b8f27910f3819da3b5c1890e08
Max <ppmax@bk.ru>
2023-03-21 00:13:24 +0300

    Remove modal created

--- frontend/src/pages/modals/Remove.jsx
+++ frontend/src/pages/modals/Remove.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
 import { Modal, FormGroup } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import { useSelector, useDispatch } from 'react-redux';
+import { setActiveChannel } from '../../store/activeChannelSlice';
+import { removeMessages } from '../../store/messagesSlice';
 
-const generateOnSubmit = ({ modalInfo, setItems, onHide }) => (e) => {
-  e.preventDefault();
-  setItems((items) => items.filter((i) => i.id !== modalInfo.item.id));
-  onHide();
-};
+const Remove = ({ socket, onHide, modalInfo }) => {
+  const activeChannelId = useSelector((state) => state.channel.activeChannel).id;
+  const messages = useSelector((state) => state.messages.allMessages);
+  const dispatch = useDispatch();
 
-const Remove = (props) => {
-  const { onHide } = props;
-  const onSubmit = generateOnSubmit(props);
+  const f = useFormik({
+    initialValues: {
+      removingChannelId: modalInfo.item.id,
+    },
+    onSubmit: () => {
+      socket.sendRemovedChannel({ id: f.values.removingChannelId });
+      if (activeChannelId === f.values.removingChannelId) {
+        dispatch(setActiveChannel({ name: 'general', id: 1 }));
+      }
+      dispatch(removeMessages({ id: f.values.removingChannelId }));
+      f.resetForm();
+      onHide();
+    },
+  });
+
+  console.log(messages);
+  console.log(f.values.removingChannelId);
+  console.log(activeChannelId);
 
   return (
-    <Modal show>
+    <Modal show centered>
       <Modal.Header closeButton onHide={onHide}>
-        <Modal.Title>Remove</Modal.Title>
+        <Modal.Title className="p-1">Удалить канал</Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-        <form onSubmit={onSubmit}>
-          <FormGroup>
-            <input type="submit" className="btn btn-danger mt-2" value="remove" />
+        <p className="lead p-1">Уверены?</p>
+        <form onSubmit={f.handleSubmit}>
+          <FormGroup className="d-flex justify-content-start">
+            <input type="submit" className="btn btn-danger" value="Удалить" />
+            <input onClick={() => onHide()} type="submit" className="me-2 btn btn-secondary ms-2" value="Отменить" />
           </FormGroup>
         </form>
       </Modal.Body>
