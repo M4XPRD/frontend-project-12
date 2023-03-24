a494a40b3c0e8d357941d195ee303f19ad528c95
Max <ppmax@bk.ru>
2023-03-25 00:10:31 +0300

    activeChannel merged with channels slice

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import channelsReducer from './channelsSlice';
-import activeChannelReducer from './activeChannelSlice';
 import messagesReducer from './messagesSlice';
 
 const store = configureStore({
   reducer: {
     channels: channelsReducer,
-    activeChannel: activeChannelReducer,
     messages: messagesReducer,
   },
 });
