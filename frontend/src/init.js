5ef37300d400f045b5b88e9eace2b62a33876628
Max <ppmax@bk.ru>
2023-03-29 05:45:43 +0300

    Rollbar installed

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import React from 'react';
+import { Provider as RollbalProvider, ErrorBoundary } from '@rollbar/react';
 import i18n from './i18n';
 import App from './App';
 import './index.css';
@@ -11,6 +12,11 @@ import {
   addChannel, removeChannel, renameChannel,
 } from './store/channelsSlice';
 import store from './store/index';
+
+const rollbarConfig = {
+  accessToken: 'post_server_item access token',
+  environment: 'production',
+};
 
 const Init = (socket) => {
   socket.on('newMessage', (data) => {
@@ -30,11 +36,15 @@ const Init = (socket) => {
   return (
     <React.StrictMode>
       <BrowserRouter>
-        <Provider store={store}>
-          <I18nextProvider i18n={i18n}>
-            <App socket={socket} />
-          </I18nextProvider>
-        </Provider>
+        <RollbalProvider config={rollbarConfig}>
+          <ErrorBoundary>
+            <Provider store={store}>
+              <I18nextProvider i18n={i18n}>
+                <App socket={socket} />
+              </I18nextProvider>
+            </Provider>
+          </ErrorBoundary>
+        </RollbalProvider>
       </BrowserRouter>
     </React.StrictMode>
   );
