219cac01c12399124789a39d4ca6993002c685b2
Max <ppmax@bk.ru>
2023-03-29 06:42:53 +0300

    Folder renamed from "pages" to "components"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Main from './pages/Main';
+import Main from './components/Main';
 import AuthProvider from './contexts/AuthProvider';
 import NetworkProvider from './contexts/NetworkProvider';
 import SocketProvider from './contexts/SocketProvider';
