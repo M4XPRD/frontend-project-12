47bf60eec83adb7a21c246be57ff45ad2ab5979e
Max <ppmax@bk.ru>
2023-04-03 15:03:04 +0300

    New routes added

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -2,7 +2,8 @@ const apiPath = '/api/v1';
 
 export default {
   loginPath: () => [apiPath, 'login'].join('/'),
-  loginPage: () => ['login'].join('/'),
   dataPath: () => [apiPath, 'data'].join('/'),
   usersPath: () => [apiPath, 'signup'].join('/'),
+  loginPage: () => '/login',
+  mainPage: () => '/',
 };
